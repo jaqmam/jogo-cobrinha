fea12fb786f208e8da1537d36fbb7845eb76ad06
jaqmam <jaqmam@gmail.com>
2021-09-18 12:18:13 -0300

    criar comida

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+//array da cobrinha
 snake[0] = {
     x: 8 *box,
-    y: 8* box
+    y: 8 * box
 }
 let direction = "right";
 
+//array da comida
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+//função para criar o bckground onde o jogo vai acontecer
 function criarBG(){
     context.fillStyle = "lightgreen"
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+//Função para criar a cobrinha
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
@@ -20,8 +29,14 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown',update);
 
+//funcão para acionamento da direção da cobrinha
 function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left";    
     if(event.keyCode == 38 && direction != "down") direction = "up";
@@ -30,6 +45,8 @@ function update(event){
 }
 
 function iniciarJogo(){
+
+    //Não permitir que cobrinha suma da tela
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -37,6 +54,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
